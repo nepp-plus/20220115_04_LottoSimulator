4bf22ef81617bd7069761dedb265fdda0be3bcb3
nepp.plus <nepp.plus@gmail.com>
2022-01-15 13:47:02 +0900

    Bubble Sort를 이용해, 당첨번호 정렬

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -139,6 +139,35 @@ public class MainDrive {
 		}
 		
 		
+//		당첨번호 오름차순 정렬 - Bubble Sort
+//		동작속도는 느리지만, 코딩하기가 편한 로직.
+		
+		for (int i=0 ; i < winNumberArr.length ; i++) {
+			
+//			j가 5인 경우 => j+1은 6임. => 배열 범위를 벗어남.
+//			j가 길이 보다 2개 적은값 까지만 돌도록.
+			for (int j=0 ; j < winNumberArr.length-1 ; j++) {
+				
+//				이어져있는 2개의 숫자를 비교, 순서가 잘못되어있다면 바꿔주자.
+				
+				if (winNumberArr[j] > winNumberArr[j+1]) {
+					
+//					두 변수의 값 교환.
+					
+					int backUp = winNumberArr[j];
+					
+					winNumberArr[j] = winNumberArr[j+1];
+					
+					winNumberArr[j+1] = backUp;
+					
+				}
+				
+				
+			}
+			
+		}
+		
+		
 //		확인용 - 당첨번호가 어떻게나왔는지?
 		for (int winNum :  winNumberArr) {
 			System.out.println(winNum);
